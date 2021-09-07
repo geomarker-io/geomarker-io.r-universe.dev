4ac17b25fe0fecc33e06299e0bd6de6f68beeb9e
Erika <erika.rasnick@gmail.com>
2021-09-07 11:19:34 -0400

    add a few more packages

---
+++ make_packages_json.R
@@ -0,0 +1,17 @@
+library(dplyr)
+
+df <- tibble::tibble(
+  package = c("addPmData", "addNarrData",
+              "schwartzGeohashPM", "addNarrData", "OfflineGeocodeR"))
+
+
+
+
+              # ,"schwartzGeohashPM", "addNarrData", "OfflineGeocodeR",
+              # "s3", "zctaDB", "addNlcdData", "airPb", "ecat", "aiR",
+              # "tidydlnm", "hamilton"))
+
+df  <- df %>%
+  mutate(url = glue::glue("https://github.com/geomarker-io/{package}"))
+
+jsonlite::write_json(df, 'packages.json', pretty = TRUE)
